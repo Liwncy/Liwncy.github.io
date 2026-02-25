4eb33e1d5486638396b7a4f1f263e44bef1d488b
wanchang.li <liwncy@qq.com>
2026-02-25 17:21:52 +0800

    update:2026年2月25日17:21:45

---
+++ assets/empty-8F1_zbj3.js
@@ -0,0 +1,69 @@
+import{j as p,n as m,a as y,b as s,g as n,w as o,h as t}from"./index-3I5B3wp3.js";const f={class:"markdown-body"},x={style:{display:"flex","justify-content":"space-between","margin-top":"20px"}},v={},E={__name:"empty",setup(_,{expose:r}){return r({frontmatter:{}}),(w,a)=>{const g=p("lay-anchor"),e=p("lay-field"),l=p("lay-empty"),c=p("lay-code"),d=p("lay-button"),u=p("lay-table-box"),k=p("lay-icon"),i=p("router-link");return y(),m("div",f,[s(g,{anchors:"基本介绍,基础使用,指定描述,扩展插槽,图片插槽,Empty 属性,Empty 插槽",currIndex:-1,show:!0}),s(e,{id:"基本介绍",title:"基本介绍",style:{"margin-top":"21px","margin-bottom":"20px"}}),a[10]||(a[10]=n("div",{class:"describe-plugin"},[n("p",null,"当目前没有数据时，用于显式的用户提示。")],-1)),s(e,{id:"基础使用",title:"基础使用",style:{"margin-top":"21px","margin-bottom":"20px"}}),s(c,null,{code:o(()=>[...a[0]||(a[0]=[n("pre",null,[n("code",{"v-pre":""},[n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("template")]),n("span",{class:"token punctuation"},">")]),t(`
+  `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("lay-empty")]),n("span",{class:"token punctuation"},">")]),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("lay-empty")]),n("span",{class:"token punctuation"},">")]),t(`
+`),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("template")]),n("span",{class:"token punctuation"},">")]),t(`
+
+`),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("script")]),n("span",{class:"token punctuation"},">")]),n("span",{class:"token script"},[n("span",{class:"token language-javascript"},[t(`
+`),n("span",{class:"token keyword"},"import"),t(),n("span",{class:"token punctuation"},"{"),t(" ref "),n("span",{class:"token punctuation"},"}"),t(),n("span",{class:"token keyword"},"from"),t(),n("span",{class:"token string"},"'vue'"),t(`
+
+`),n("span",{class:"token keyword"},"export"),t(),n("span",{class:"token keyword"},"default"),t(),n("span",{class:"token punctuation"},"{"),t(`
+  `),n("span",{class:"token function"},"setup"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),t(),n("span",{class:"token punctuation"},"{"),t(`
+
+    `),n("span",{class:"token keyword"},"return"),t(),n("span",{class:"token punctuation"},"{"),t(`
+    `),n("span",{class:"token punctuation"},"}"),t(`
+  `),n("span",{class:"token punctuation"},"}"),t(`
+`),n("span",{class:"token punctuation"},"}"),t(`
+`)])]),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("script")]),n("span",{class:"token punctuation"},">")]),t(`
+`)])],-1)])]),default:o(()=>[s(l)]),_:1}),s(e,{id:"指定描述",title:"指定描述",style:{"margin-top":"21px","margin-bottom":"20px"}}),s(c,null,{code:o(()=>[...a[1]||(a[1]=[n("pre",null,[n("code",{"v-pre":""},[n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("template")]),n("span",{class:"token punctuation"},">")]),t(`
+  `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("lay-empty")]),t(),n("span",{class:"token attr-name"},"description"),n("span",{class:"token attr-value"},[n("span",{class:"token punctuation attr-equals"},"="),n("span",{class:"token punctuation"},'"'),t("刷新试试"),n("span",{class:"token punctuation"},'"')]),n("span",{class:"token punctuation"},">")]),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("lay-empty")]),n("span",{class:"token punctuation"},">")]),t(`
+`),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("template")]),n("span",{class:"token punctuation"},">")]),t(`
+
+`),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("script")]),n("span",{class:"token punctuation"},">")]),n("span",{class:"token script"},[n("span",{class:"token language-javascript"},[t(`
+`),n("span",{class:"token keyword"},"import"),t(),n("span",{class:"token punctuation"},"{"),t(" ref "),n("span",{class:"token punctuation"},"}"),t(),n("span",{class:"token keyword"},"from"),t(),n("span",{class:"token string"},"'vue'"),t(`
+
+`),n("span",{class:"token keyword"},"export"),t(),n("span",{class:"token keyword"},"default"),t(),n("span",{class:"token punctuation"},"{"),t(`
+  `),n("span",{class:"token function"},"setup"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),t(),n("span",{class:"token punctuation"},"{"),t(`
+
+    `),n("span",{class:"token keyword"},"return"),t(),n("span",{class:"token punctuation"},"{"),t(`
+    `),n("span",{class:"token punctuation"},"}"),t(`
+  `),n("span",{class:"token punctuation"},"}"),t(`
+`),n("span",{class:"token punctuation"},"}"),t(`
+`)])]),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("script")]),n("span",{class:"token punctuation"},">")]),t(`
+`)])],-1)])]),default:o(()=>[s(l,{description:"刷新试试"})]),_:1}),s(e,{id:"扩展插槽",title:"扩展插槽",style:{"margin-top":"21px","margin-bottom":"20px"}}),s(c,null,{code:o(()=>[...a[2]||(a[2]=[n("pre",null,[n("code",{"v-pre":""},[n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("template")]),n("span",{class:"token punctuation"},">")]),t(`
+  `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("lay-empty")]),t(),n("span",{class:"token attr-name"},"description"),n("span",{class:"token attr-value"},[n("span",{class:"token punctuation attr-equals"},"="),n("span",{class:"token punctuation"},'"'),t("刷新试试"),n("span",{class:"token punctuation"},'"')]),n("span",{class:"token punctuation"},">")]),t(`
+    `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("template")]),t(),n("span",{class:"token attr-name"},"#extra"),n("span",{class:"token punctuation"},">")]),t(`
+      `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("lay-button")]),n("span",{class:"token punctuation"},">")]),t("刷新页面"),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("lay-button")]),n("span",{class:"token punctuation"},">")]),t(`
+    `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("template")]),n("span",{class:"token punctuation"},">")]),t(` 
+  `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("lay-empty")]),n("span",{class:"token punctuation"},">")]),t(`
+`),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("template")]),n("span",{class:"token punctuation"},">")]),t(`
+
+`),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("script")]),n("span",{class:"token punctuation"},">")]),n("span",{class:"token script"},[n("span",{class:"token language-javascript"},[t(`
+`),n("span",{class:"token keyword"},"import"),t(),n("span",{class:"token punctuation"},"{"),t(" ref "),n("span",{class:"token punctuation"},"}"),t(),n("span",{class:"token keyword"},"from"),t(),n("span",{class:"token string"},"'vue'"),t(`
+
+`),n("span",{class:"token keyword"},"export"),t(),n("span",{class:"token keyword"},"default"),t(),n("span",{class:"token punctuation"},"{"),t(`
+  `),n("span",{class:"token function"},"setup"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),t(),n("span",{class:"token punctuation"},"{"),t(`
+
+    `),n("span",{class:"token keyword"},"return"),t(),n("span",{class:"token punctuation"},"{"),t(`
+    `),n("span",{class:"token punctuation"},"}"),t(`
+  `),n("span",{class:"token punctuation"},"}"),t(`
+`),n("span",{class:"token punctuation"},"}"),t(`
+`)])]),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("script")]),n("span",{class:"token punctuation"},">")]),t(`
+`)])],-1)])]),default:o(()=>[s(l,{description:"刷新试试"},{extra:o(()=>[s(d,null,{default:o(()=>[...a[3]||(a[3]=[t("刷新页面",-1)])]),_:1})]),_:1})]),_:1}),s(e,{id:"图片插槽",title:"图片插槽",style:{"margin-top":"21px","margin-bottom":"20px"}}),s(c,null,{code:o(()=>[...a[4]||(a[4]=[n("pre",null,[n("code",{"v-pre":""},[n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("template")]),n("span",{class:"token punctuation"},">")]),t(`
+  `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("lay-empty")]),t(),n("span",{class:"token attr-name"},"description"),n("span",{class:"token attr-value"},[n("span",{class:"token punctuation attr-equals"},"="),n("span",{class:"token punctuation"},'"'),t("刷新试试"),n("span",{class:"token punctuation"},'"')]),n("span",{class:"token punctuation"},">")]),t(`
+    `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("template")]),t(),n("span",{class:"token attr-name"},"#image"),n("span",{class:"token punctuation"},">")]),t(`
+      图片
+    `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("template")]),n("span",{class:"token punctuation"},">")]),t(`
+  `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("lay-empty")]),n("span",{class:"token punctuation"},">")]),t(`
+`),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("template")]),n("span",{class:"token punctuation"},">")]),t(`
+
+`),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("script")]),n("span",{class:"token punctuation"},">")]),n("span",{class:"token script"},[n("span",{class:"token language-javascript"},[t(`
+`),n("span",{class:"token keyword"},"import"),t(),n("span",{class:"token punctuation"},"{"),t(" ref "),n("span",{class:"token punctuation"},"}"),t(),n("span",{class:"token keyword"},"from"),t(),n("span",{class:"token string"},"'vue'"),t(`
+
+`),n("span",{class:"token keyword"},"export"),t(),n("span",{class:"token keyword"},"default"),t(),n("span",{class:"token punctuation"},"{"),t(`
+  `),n("span",{class:"token function"},"setup"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),t(),n("span",{class:"token punctuation"},"{"),t(`
+
+    `),n("span",{class:"token keyword"},"return"),t(),n("span",{class:"token punctuation"},"{"),t(`
+    `),n("span",{class:"token punctuation"},"}"),t(`
+  `),n("span",{class:"token punctuation"},"}"),t(`
+`),n("span",{class:"token punctuation"},"}"),t(`
+`)])]),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("script")]),n("span",{class:"token punctuation"},">")]),t(`
+`)])],-1)])]),default:o(()=>[s(l,{description:"刷新试试"},{image:o(()=>[...a[5]||(a[5]=[t(" 图片 ",-1)])]),_:1})]),_:1}),s(e,{id:"Empty 属性",title:"Empty 属性",style:{"margin-top":"21px","margin-bottom":"20px"}}),s(u,null,{default:o(()=>[...a[6]||(a[6]=[n("table",null,[n("thead",null,[n("tr",null,[n("th",null,"属性"),n("th",null,"描述"),n("th",null,"可选值")])]),n("tbody",null,[n("tr",null,[n("td",null,"description"),n("td",null,"描述信息"),n("td",null,"–")])])],-1)])]),_:1}),s(e,{id:"Empty 插槽",title:"Empty 插槽",style:{"margin-top":"21px","margin-bottom":"20px"}}),s(u,null,{default:o(()=>[...a[7]||(a[7]=[n("table",null,[n("thead",null,[n("tr",null,[n("th",null,"属性"),n("th",null,"描述"),n("th",null,"可选值")])]),n("tbody",null,[n("tr",null,[n("td",null,"extra"),n("td",null,"扩展插槽"),n("td",null,"–")])])],-1)])]),_:1}),n("div",x,[n("div",null,[s(i,{to:"/zh-CN/components/avatar",class:"lay-link",style:{display:""}},{default:o(()=>[s(k,{type:"layui-icon-left"}),a[8]||(a[8]=t("头像",-1))]),_:1})]),n("div",null,[s(i,{to:"/zh-CN/components/page",class:"lay-link",style:{display:""}},{default:o(()=>[a[9]||(a[9]=t("分页 ",-1)),s(k,{type:"layui-icon-right"})]),_:1})])])])}}};export{E as default,v as frontmatter};
